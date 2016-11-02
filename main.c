888cef94721826329c4c6987e4c14803212f32b4
SUN QIONGSEN <3075921643@qq.com>
2016-11-02 12:46:34 +0800

    结构缩减

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    //c部分的测试：
+    //lplist asd;
+    LPLT point=init();
+    addnode(point);
+
+    /*c++部分的测试。
+    lplist asd=new lplist();
+
+
+    */
+    return 0;
+}
