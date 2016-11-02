9e91982a31d9815bcad2b1fcd2822d3befdb1f81
SUN QIONGSEN <3075921643@qq.com>
2016-11-02 12:47:31 +0800

    Update main.c

--- main.c
+++ main.c
@@ -5,12 +5,6 @@ int main()
     //c部分的测试：
     //lplist asd;
     LPLT point=init();
-    addnode(point);
-
-    /*c++部分的测试。
-    lplist asd=new lplist();
-
-
-    */
+    addnode(point)；
     return 0;
 }
